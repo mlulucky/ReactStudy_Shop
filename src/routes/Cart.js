320d54a6f5530de22a9894dd7773b37d5ba0a65b
mlulucky <dmswjdans@gmail.com>
2023-07-10 00:28:58 +0900

    Redux state 데이터 바인딩구현 및 state 변경 준비중

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,24 +1,46 @@
-import {Table} from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
+import { useSelector } from 'react-redux'
 
-export default function Cart(){
-    return (
-        <Table bordered>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>상품명</th>
-          <th>수량</th>
-          <th>변경하기</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-      </tbody>
-    </Table>
-    )
+
+export default function Cart() {
+  // Redux 에 있는 state 를 가져오는 함수
+  let commonState = useSelector((state) => { return state })
+  console.log(commonState);
+  console.log("작명2", commonState.작명2);
+
+  return (
+    <div className='cont'>
+      <Table bordered>
+        <thead>
+          <tr>
+            <th>상품 id</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>변경하기</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            commonState.cart.map((a, i) =>
+              // { return   }  -> 중괄호 return 생략 가능
+              <Content key={i} 카트={commonState} 인덱스={i} />
+
+            )
+          }
+        </tbody>
+      </Table>
+    </div>
+
+  )
+}
+
+function Content({ 카트, 인덱스 }) {
+  return (
+    <tr>
+      <td>{카트.cart[인덱스].id}</td>
+      <td>{카트.cart[인덱스].name}</td>
+      <td>{카트.cart[인덱스].count}</td>
+      <td>@mdo</td>
+    </tr>
+  )
 }
--- src/store.js
+++ src/store.js
@@ -1,6 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-// store 란 ? state 들을 보관해놓는 통
+// Redux 란 ? state 들을 보관해놓는 통
+
+// state 를 만들어주는 함수
+let firstState = createSlice({ // useState 와 비슷한 기능
+    name : "myName",
+    initialState : "moon"
+})
+
+let secondState = createSlice({
+    name : "arr",
+    initialState : [0,1,2]
+})
+
+let cart = createSlice({
+    name : "myFavorite",
+    initialState : [
+        {id : 0, name : 'White and Black', count : 2},
+        {id : 2, name : 'Grey Yordan', count : 1}
+    ]
+})
+
+// state 를 등록하는 함수
 export default configureStore({
-    reducer: {}
+    reducer: {
+        작명 : firstState.reducer,
+        작명2 : secondState.reducer,
+        cart : cart.reducer
+    }
 })
